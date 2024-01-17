a03fb15d2eb5a3d73273d0361f6fd2d661b4dc94
Andres <andresberejnoi@gmail.com>
2024-01-17 13:47:58 -0500

    added map to display population clusters

--- Housing Prices/Home.py
+++ Housing Prices/Home.py
@@ -1,8 +1,20 @@
 import streamlit as st
 import pandas as pd 
+from matplotlib import colormaps
 
 
 df = pd.read_csv('datasets/housing.csv')
+df = df[:len(df)//2]  #reducing the dataframe in half
 
 st.markdown("# Raw Housing Data")
-df
+st.dataframe(df)
+
+#display a simple map
+st.map(
+    df, 
+    longitude='longitude', 
+    latitude='latitude', 
+    size='population',
+    #color='population',
+    
+)
