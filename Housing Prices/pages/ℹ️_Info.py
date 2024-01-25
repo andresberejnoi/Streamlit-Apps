f7d3eef7ef76483cb9af86e731f566eb2e73926c
Andres <andresberejnoi@gmail.com>
2024-01-24 22:03:26 -0500

    added links to elements used in this project

--- Housing Prices/pages/ℹ️_Info.py
+++ Housing Prices/pages/ℹ️_Info.py
@@ -3,4 +3,13 @@ import streamlit as st
 st.title("Project Information")
 
 st.markdown("This is an example project to show the basics of Streamlit.")
-st.markdown("Here is a [blog post](#) explaining how this page was made.")
+st.markdown("Here is a [blog post](#) explaining how this page was made. Additionally, here is the link to the [Github repository](https://github.com/andresberejnoi/Streamlit-Apps/tree/main/Housing%20Prices) for the project.")
+
+st.markdown("""
+    ### Streamlit Elements Used:
+* [st.data_editor](https://docs.streamlit.io/library/api-reference/data/st.data_editor)
+* [st.map](https://docs.streamlit.io/library/api-reference/charts/st.map)
+* [st.form](https://docs.streamlit.io/library/api-reference/control-flow/st.form)
+* [st.text_area](https://docs.streamlit.io/library/api-reference/widgets/st.text_area)
+* [st.bar_chart](https://docs.streamlit.io/library/api-reference/charts/st.bar_chart)
+""")
